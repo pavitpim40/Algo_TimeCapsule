7822669f707a1df423aeca7f9c6a4b928996456e
pavitpim40_mbp <pavitpim@gmail.com>
2024-01-04 20:36:45 +0700

    missing letter

---
+++ free-code-camp/intermediate-algorithm-scripting/9-missing-letter/missing-letter.js
@@ -0,0 +1,7 @@
+function fearNotLetter(str) {
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str.charCodeAt(i + 1) - str.charCodeAt(i) > 1)
+      return String.fromCharCode(str.charCodeAt(i) + 1);
+  }
+}
+fearNotLetter('abce');
