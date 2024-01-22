d2d714be247c10b29e5291498cc7e9a62a5f54a8
pavitpim40_mbp <pavitpim@gmail.com>
2024-01-23 01:19:50 +0700

    correct:cashRegister

--- free-code-camp/algorithm-project/5-cash-register/cashRegister.js
+++ free-code-camp/algorithm-project/5-cash-register/cashRegister.js
@@ -1,17 +1,3 @@
-function initChangeArr() {
-  return [
-    ['PENNY', 0],
-    ['NICKEL', 0],
-    ['DIME', 0],
-    ['QUARTER', 0],
-    ['ONE', 0],
-    ['FIVE', 0],
-    ['TEN', 0],
-    ['TWENTY', 0],
-    ['ONE HUNDRED', 0],
-  ];
-}
-
 function createOutput(status, changeArr) {
   return { status, change: changeArr };
 }
@@ -20,54 +6,65 @@ function calBalanceInMachine(cid) {
   return cid.reduce((a, c) => a + c[1], 0);
 }
 
-function selectIndex(change) {
-  if (change >= 100) return { index: 8, value: 100 };
-  else if (change >= 20) return { index: 7, value: 20 };
-  else if (change >= 10) return { index: 6, value: 10 };
-  else if (change >= 5) return { index: 5, value: 5 };
-  else if (change >= 1) return { index: 4, value: 1 };
-  else if (change >= 0.25) return { index: 3, value: 0.25 };
-  else if (change >= 0.1) return { index: 2, value: 0.1 };
-  else if (change >= 0.05) return { index: 1, value: 0.05 };
-  else if (change >= 0.01) return { index: 0, value: 0.01 };
-  else return { index: -1, value: 0 };
+function selectCashTypeArr(change, cid) {
+  for (let i = cid.length - 1; i >= 0; i--) {
+    let unitValue = convertUnitToValue(cid[i][0]);
+    let balanceValue = cid[i][1];
+    // console.log("AA",unitValue, change,balanceValue)
+    if (balanceValue !== 0 && unitValue <= change) return cid[i];
+  }
 }
+
+function convertUnitToValue(unit) {
+  switch (unit) {
+    case 'ONE HUNDRED':
+      return 100;
+    case 'TWENTY':
+      return 20;
+    case 'TEN':
+      return 10;
+    case 'FIVE':
+      return 5;
+    case 'ONE':
+      return 1;
+    case 'QUARTER':
+      return 0.25;
+    case 'DIME':
+      return 0.1;
+    case 'NICKEL':
+      return 0.05;
+    case 'PENNY':
+      return 0.01;
+    default:
+      return 0;
+  }
+}
+
+function reduceCash(cashArr, value) {
+  cashArr[1] -= value;
+}
+
 function checkCashRegister(price, cash, cid) {
   let change = cash - price;
   let allChange = calBalanceInMachine(cid);
   if (allChange < change) return createOutput('INSUFFICIENT_FUNDS', []);
   else if (allChange == change) return createOutput('CLOSED', cid);
 
-  const changeArr = initChangeArr();
-  let cutObject = selectIndex(change);
+  const changeArr = [];
+  while (change != 0) {
+    let cashArr = selectCashTypeArr(change, cid);
+    if (!cashArr) return createOutput('INSUFFICIENT_FUNDS', []);
 
-  do {
-    let cutIndex = cutObject.index;
-    let value = cutObject.value;
-    console.log(change, cutIndex, cid[cutIndex]);
-    if (cutIndex == -1) {
-      return createOutput('INSUFFICIENT_FUNDS', []);
-    }
+    let unitName = cashArr[0];
+    let value = convertUnitToValue(unitName);
 
-    if (cutIndex != -1 && cid[cutIndex][1] !== 0) {
-      change -= value;
-      changeArr[cutIndex][1] += value;
-      cutObject = selectIndex(change);
-    } else {
-      cutIndex--;
-    }
-  } while (change != 0 || cutObject.index != -1);
+    let foundIndex = changeArr.findIndex((el) => el[0] == unitName);
+    if (foundIndex != -1) changeArr[foundIndex][1] += value;
+    else changeArr.push([unitName, value]);
+
+    reduceCash(cashArr, value);
+    change = +(change - value).toFixed(2);
+  }
 
   return createOutput('OPEN', changeArr);
 }
-checkCashRegister(19.5, 20, [
-  ['PENNY', 1.01],
-  ['NICKEL', 2.05],
-  ['DIME', 3.1],
-  ['QUARTER', 4.25],
-  ['ONE', 90],
-  ['FIVE', 55],
-  ['TEN', 20],
-  ['TWENTY', 60],
-  ['ONE HUNDRED', 100],
-]);
