0ad7e0b0ce9928e5a27dd60639fcb3ede36a58de
pavitpim40_mbp <pavitpim@gmail.com>
2024-01-27 19:58:50 +0700

    review dfs

--- depth-fist-search/02-visible-tree-node/visible-tree-node.js
+++ depth-fist-search/02-visible-tree-node/visible-tree-node.js
@@ -1,3 +1,4 @@
+// 2 : State ที่ต้องส่งไปคือค่าสูงสุดที่ส่งไป (Max So Far)
 function countVisibleNode(root, maxSofar) {
   if (root == null) return 0;
   let total = 0;
@@ -6,6 +7,7 @@ function countVisibleNode(root, maxSofar) {
   if (root.val > maxSofar) maxSofar = root.val;
   let cL = countVisibleNode(root.left, maxSofar);
   let cR = countVisibleNode(root.right, maxSofar);
+  // 1 : state ที่ return คือค่า total
   return total + cL + cR;
 }
 
