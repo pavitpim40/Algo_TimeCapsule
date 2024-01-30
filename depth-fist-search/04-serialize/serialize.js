b85ad0e012d56586c532b4ef7460b9d6a2050a33
pavitpim40_mbp <pavitpim@gmail.com>
2024-01-30 14:22:45 +0700

    deserialize

--- depth-fist-search/04-serialize/serialize.js
+++ depth-fist-search/04-serialize/serialize.js
@@ -7,3 +7,20 @@ function serialize(root) {
   result += ' ' + serialize(root.right);
   return result;
 }
+
+function deserialize_recurse(nodeArr) {
+  if (nodeArr.lenght == 0) return null;
+  if (nodeArr[0] === 'x') return null;
+  const tree = new Node(
+    nodeArr[0],
+    deserialize_recurse(nodeArr.slice(1)),
+    deserialize_recurse(nodeArr.slice(2))
+  );
+  console.log(tree);
+  return tree;
+}
+
+function deserialize(s) {
+  // AND HERE
+  return deserialize_recurse(s.split(' '));
+}
